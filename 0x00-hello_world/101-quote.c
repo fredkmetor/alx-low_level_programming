19f99c4cc92bb73fd648f90aaacd410271f4d584
fredkmetor <metorfrederick@gmail.com>
2022-10-30 20:53:12 +0000

    print text to standard output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry piont
+ *Print exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+printf("\n");
+
+return (1);
+}
